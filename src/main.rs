3e2fbcc331d13ab2eb11aafb8ff5388d491306ce
Salih Karabulut <karabulut18@itu.edu.tr>
2025-10-30 18:37:42 +0300

    [exit is provided]

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,44 @@
-#[allow(unused_imports)]
 use std::io::{self, Write};
+
 
 fn main()
 {
-
     loop
     {
         print!("$ ");
         io::stdout().flush().unwrap();    
-        let mut command = String::new();
-        io::stdin().read_line(&mut command).unwrap();
-        println!("{}: command not found", command.trim());
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        let mut parts = input.trim().split_whitespace();
+        let command = parts.next();
+        match command
+        {
+            Some("exit") =>
+            {
+                if let Some(arg) = parts.next()
+                {
+                    if let Ok(exit_code) = arg.parse::<i32>()
+                    {
+                        std::process::exit(exit_code);
+                    }
+                    else
+                    {
+                        std::process::exit(1);
+                    }
+                }
+                else
+                {
+                    std::process::exit(0);
+                }
+            }
+            Some(cmd) =>
+            {
+                println!("{}: command not found", cmd.trim());
+            }
+            None =>
+            {
+                continue;
+            }
+        }
     }
 }
