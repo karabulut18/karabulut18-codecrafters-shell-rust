1ee032139f80391d93f1584fae9cfba5b36082a5
Salih Karabulut <karabulut18@itu.edu.tr>
2025-10-30 18:11:53 +0300

    [loop]

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,13 @@ use std::io::{self, Write};
 
 fn main()
 {
-    print!("$ ");
-    io::stdout().flush().unwrap();
 
-    let mut command = String::new();
-    io::stdin().read_line(&mut command).unwrap();
-    println!("{}: command not found", command.trim());
+    loop
+    {
+        print!("$ ");
+        io::stdout().flush().unwrap();    
+        let mut command = String::new();
+        io::stdin().read_line(&mut command).unwrap();
+        println!("{}: command not found", command.trim());
+    }
 }
